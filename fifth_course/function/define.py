052991bbc92fb686959e444d89bec4b770f00c78
aSmallAnimal <54016009+ASmallAnimal@users.noreply.github.com>
2020-01-29 17:52:19 +0800

    tube_draw

---
+++ fifth_course/function/define.py
@@ -0,0 +1,7 @@
+#function is a realization of "IPO"
+#lambda fun
+f=lambda x,y:x+y
+print(f(10,15))
+
+f1 = lambda: "this is a lambda function"
+print(f1())
---
+++ fifth_course/review.md
@@ -0,0 +1,7 @@
+# function
+
+1. abs()-->|x|
+2. divmod(a,b)-->divide+mod
+3. pow()
+4. round()--> <0.5=0,>=0.5=1
+5. max(),min()
